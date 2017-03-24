38e17940cf6202b5880e6af652a8fd06207411a9
Ben McLean <benmclean007@gmail.com>
2017-03-24 19:31:12 +1100

    Added collection support to abstract ProtobufController

--- src/com/arctro/ssn/backend/controllers/ProtobufController.java
+++ src/com/arctro/ssn/backend/controllers/ProtobufController.java
@@ -1,9 +1,10 @@
 package com.arctro.ssn.backend.controllers;
 
+import com.arctro.ssn.protobuf.models.ProtobufCollectionWrapper;
 import com.arctro.ssn.protobuf.models.ProtobufWrapper;
 import com.arctro.ssn.supporting.SSNContext;
 
-public abstract class ProtobufController<T extends ProtobufWrapper<?>> {
+public abstract class ProtobufController<S extends ProtobufWrapper<?>, C extends ProtobufCollectionWrapper<?>> {
 	
 	SSNContext context;
 	
