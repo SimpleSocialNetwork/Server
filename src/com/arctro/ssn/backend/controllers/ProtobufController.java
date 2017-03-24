8abf6c156107b1b1f9ec00fa31a99f254641549f
Ben McLean <benmclean007@gmail.com>
2017-03-24 19:48:23 +1100

    Added basic abstract functions to ProtobufController

--- src/com/arctro/ssn/backend/controllers/ProtobufController.java
+++ src/com/arctro/ssn/backend/controllers/ProtobufController.java
@@ -1,10 +1,10 @@
 package com.arctro.ssn.backend.controllers;
 
 import com.arctro.ssn.protobuf.models.ProtobufCollectionWrapper;
-import com.arctro.ssn.protobuf.models.ProtobufWrapper;
+import com.arctro.ssn.protobuf.models.ServableProtobufWrapper;
 import com.arctro.ssn.supporting.SSNContext;
 
-public abstract class ProtobufController<S extends ProtobufWrapper<?>, C extends ProtobufCollectionWrapper<?>> {
+public abstract class ProtobufController<S extends ServableProtobufWrapper<?>, C extends ProtobufCollectionWrapper<?>> {
 	
 	SSNContext context;
 	
@@ -12,4 +12,12 @@ public abstract class ProtobufController<S extends ProtobufWrapper<?>, C extends
 		this.context = context;
 	}
 	
+	
+	public abstract S get(int id);
+	
+	public abstract void set(S proto);
+	
+	public abstract void delete(int id);
+	
+	public abstract ResourceAccessibility isAccessible(int id);
 }
---
+++ src/com/arctro/ssn/backend/controllers/ResourceAccessibility.java
@@ -0,0 +1,7 @@
+package com.arctro.ssn.backend.controllers;
+
+public enum ResourceAccessibility {
+	EDIT,
+	VIEW,
+	NONE
+}
