9d2449550379ea8e0434ad053ec830832ca160f4
Ben McLean <benmclean007@gmail.com>
2017-03-24 19:55:49 +1100

    Added temporary encryption key

---
+++ src/com/arctro/ssn/backend/auth/AuthManagerKey.java
@@ -0,0 +1,5 @@
+package com.arctro.ssn.backend.auth;
+
+public class AuthManagerKey {
+	public static final byte[] key = new byte[]{ 'T', 'h', 'e', 'B', 'e', 's', 't', 'S', 'e', 'c', 'r','e', 't', 'K', 'e', 'y' };
+}
