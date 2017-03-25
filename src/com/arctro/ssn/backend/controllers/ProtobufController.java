0f9f16ecdefd014e87169314fb4b0008f5bec45d
Ben McLean <benmclean007@gmail.com>
2017-03-26 10:45:17 +1100

    Added new super class for ProtobufController (which is now called ServableProtobufController)

--- src/com/arctro/ssn/backend/controllers/ProtobufController.java
+++ src/com/arctro/ssn/backend/controllers/ProtobufController.java
@@ -1,17 +1,17 @@
 package com.arctro.ssn.backend.controllers;
 
 import com.arctro.ssn.protobuf.models.ProtobufCollectionWrapper;
-import com.arctro.ssn.protobuf.models.ServableProtobufWrapper;
+import com.arctro.ssn.protobuf.models.ProtobufWrapper;
 import com.arctro.ssn.supporting.SSNContext;
 
 /**
- * A common ancestor for most ProtobufControllers
+ * A common ancestor for ProtobufControllers
  * @author Ben McLean
  *
- * @param <S> The ServableProtobufWrapper that this controller controls
+ * @param <S> The ProtobufWrapper that this controller controls
  * @param <C> The Collection type of S
  */
-public abstract class ProtobufController<S extends ServableProtobufWrapper<?>, C extends ProtobufCollectionWrapper<?>> {
+public abstract class ProtobufController<S extends ProtobufWrapper<?>, C extends ProtobufCollectionWrapper<?>>  {
 	
 	SSNContext context;
 	
@@ -23,29 +23,4 @@ public abstract class ProtobufController<S extends ServableProtobufWrapper<?>, C
 		this.context = context;
 	}
 	
-	/**
-	 * Returns the S associated with the provided ID
-	 * @param id The ID of the S to get
-	 * @return The S associated with the provided ID
-	 */
-	public abstract S get(int id);
-	
-	/**
-	 * Sets/creates an S
-	 * @param proto The S to create/set
-	 */
-	public abstract void set(S proto);
-	
-	/**
-	 * Deletes the S associated with the provided ID
-	 * @param id The ID of the S to delete
-	 */
-	public abstract void delete(int id);
-	
-	/**
-	 * Checks if an S is accessible
-	 * @param id The ID of the S to check
-	 * @return If an S is accessible
-	 */
-	public abstract ResourceAccessibility isAccessible(int id);
 }
---
+++ src/com/arctro/ssn/backend/controllers/ServableProtobufController.java
@@ -0,0 +1,49 @@
+package com.arctro.ssn.backend.controllers;
+
+import com.arctro.ssn.protobuf.models.ProtobufCollectionWrapper;
+import com.arctro.ssn.protobuf.models.ServableProtobufWrapper;
+import com.arctro.ssn.supporting.SSNContext;
+
+/**
+ * A common ancestor for ServabkeProtobufControllers
+ * @author Ben McLean
+ *
+ * @param <S> The ServableProtobufWrapper that this controller controls
+ * @param <C> The Collection type of S
+ */
+public abstract class ServableProtobufController<S extends ServableProtobufWrapper<?>, C extends ProtobufCollectionWrapper<?>> extends ProtobufController<S,C>{
+	
+	/**
+	 * Initializes the ProtobufController
+	 * @param context The request's SSNContext
+	 */
+	public ServableProtobufController(SSNContext context){
+		super(context);
+	}
+	
+	/**
+	 * Returns the S associated with the provided ID
+	 * @param id The ID of the S to get
+	 * @return The S associated with the provided ID
+	 */
+	public abstract S get(int id);
+	
+	/**
+	 * Sets/creates an S
+	 * @param proto The S to create/set
+	 */
+	public abstract void set(S proto);
+	
+	/**
+	 * Deletes the S associated with the provided ID
+	 * @param id The ID of the S to delete
+	 */
+	public abstract void delete(int id);
+	
+	/**
+	 * Checks if an S is accessible
+	 * @param id The ID of the S to check
+	 * @return If an S is accessible
+	 */
+	public abstract ResourceAccessibility isAccessible(int id);
+}
